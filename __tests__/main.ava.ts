44eba4e7aff27a09f2d886dbce0f7e0ea1689737
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2022-04-15 14:20:01 +0200

    test plan for multicall and attached FTs

--- __tests__/main.ava.ts
+++ __tests__/main.ava.ts
@@ -1,4 +1,4 @@
-import { Workspace, NEAR, Gas } from 'near-workspaces-ava';
+import { Workspace, NEAR, Gas, NearAccount } from 'near-workspaces-ava';
 import { tests as adminsTests } from './admins.ava';
 import { tests as tokensTests } from './tokens.ava';
 import { tests as nearAPITests } from './nearAPI.ava';
@@ -18,28 +18,38 @@ const workspace = Workspace.init(async ({root}) => {
   const alice = await root.createAccount('alice');
   const bob = await root.createAccount('bob');
 
-  // deploy testing helper contract
-  const testHelper = await root.createAndDeploy(
-    'helper',
-    'build/test_helper_release.wasm'
-  );
-
-  // deploy multicall factory with alice admin
-  const multicallFactory = await root.createAndDeploy(
-    'factory',
-    'build/factory_release.wasm',
-    {
-      method: 'init',
-      args: {
-        init_owners: [root.accountId],
-        // set multicall instance creation fee to 0.001 NEAR
-        init_fee: NEAR.parse('1 mN'),
-        // add root as dao factory, so we can deploy an instance for alice later
-        init_factories: [root.accountId]
-      },
-      gas: Gas.parse("10 Tgas")
-    }
-  );
+  // deploy testing helper contract and multicall factory with alice admin
+  // for test token implementation, see https://github.com/ref-finance/ref-contracts/blob/22099fa4476f1d6dd94573063307783902568d63/test-token/src/lib.rs
+  const [testHelper, testToken, multicallFactory]: NearAccount[] = await Promise.all([
+    root.createAndDeploy(
+      'helper',
+      'build/test_helper_release.wasm'
+    ),
+    root.createAndDeploy(
+      'test_token',
+      '__tests__/test_contracts/test_token.wasm',
+      {
+        method: 'new',
+        args: {},
+        gas: Gas.parse("10 Tgas")
+      }
+    ),
+    root.createAndDeploy(
+      'factory',
+      'build/factory_release.wasm',
+      {
+        method: 'init',
+        args: {
+          init_owners: [root.accountId],
+          // set multicall instance creation fee to 0.001 NEAR
+          init_fee: NEAR.parse('1 mN'),
+          // add root as dao factory, so we can deploy an instance for alice later
+          init_factories: [root.accountId]
+        },
+        gas: Gas.parse("10 Tgas")
+      }
+    )
+  ]);
 
   // create a multicall instance for alice. Alice will be admin
   // pre-load multicall contract with 2 NEAR (minus factory fee) for state storage
@@ -73,7 +83,7 @@ const workspace = Workspace.init(async ({root}) => {
   );
 
   // Return accounts to be available in tests
-  return {alice, bob, multicall, testHelper};
+  return {alice, bob, multicall, testHelper, testToken};
 });
 
 // run tests
--- __tests__/multicall.ava.ts
+++ __tests__/multicall.ava.ts
@@ -1,4 +1,5 @@
 import { Workspace, NEAR, Gas } from 'near-workspaces-ava';
+import { MulticallArgs } from '../assembly/multicall/model';
 import { getFunctionCallError, encodeBase64 } from './helpers';
 
 
@@ -347,6 +348,181 @@ export function tests(workspace: Workspace) {
       && ( logs.hasOwnProperty("call_15_1 on_success") )
     );
     test.log(logs);
+  });
+  workspace.test('multicall by admin', async (test, {alice, bob, multicall, testHelper, root}) => {
+    // alice is admin so she can call multicall
+    /**
+     * do a multicall with 3 batches as following (Pseudocode):
+     *   calls = [
+     *     [ Batch_11, Batch_12 ],
+     *     [ Batch_21 ]
+     *   ]
+     * where:
+     *   Batch_11 = { address: testHelper.accountId, actions: [ call_11_1, call_11_2 ] }
+     *   Batch_12 = { address: testHelper.accountId, actions: [ call_12_1 ] }
+     *   Batch_21 = { address: testHelper.accountId, actions: [ call_21_1 ] }
+     * 
+     * 
+     * test:
+     * 1- calls in the same batch run in the same block, here: call_11_1 & call_11_2
+     * 2- Batch_21 runs parallel to Batch_11
+     * 3- Batch_12 runs after to Batch_11
+     */
+    await alice.call(
+      multicall.accountId,
+      'multicall',
+      {
+        calls: [
+          [ 
+            {
+              address: testHelper.accountId,
+              actions: [
+                {
+                  func: "log",
+                  args: encodeBase64(JSON.stringify( { msg: "call_11_1" } )),
+                  gas: "5000000000000",
+                  depo: "0"
+                },
+                {
+                  func: "log",
+                  args: encodeBase64(JSON.stringify( { msg: "call_11_2" } )),
+                  gas: "5000000000000",
+                  depo: "0"
+                }
+              ]
+            },
+            {
+              address: testHelper.accountId,
+              actions: [
+                {
+                  func: "log",
+                  args: encodeBase64(JSON.stringify( { msg: "call_12_1" } )),
+                  gas: "5000000000000",
+                  depo: "0"
+                }
+              ]
+            }
+          ],
+          [
+            {
+              address: testHelper.accountId,
+              actions: [
+                {
+                  func: "log",
+                  args: encodeBase64(JSON.stringify( { msg: "call_21_1" } )),
+                  gas: "5000000000000",
+                  depo: "0"
+                }
+              ]
+            }
+          ]
+        ]
+      },
+      {
+        gas: Gas.parse('70 Tgas'),
+        attachedDeposit: NEAR.from('1') // 1 yocto
+      }
+    );
+
+    const map_entries: {key: string, value: string}[] = await testHelper.view("get_logs", {});
+    const logs = {};
+    for (let i = 0; i < map_entries.length; i++) {
+      logs[map_entries[i].key] = map_entries[i].value;
+    }
+
+    const call_11_1_block: bigint = BigInt( logs["call_11_1"] );
+    const call_11_2_block: bigint = BigInt( logs["call_11_2"] );
+    const call_12_1_block: bigint = BigInt( logs["call_12_1"] );
+    const call_21_1_block: bigint = BigInt( logs["call_21_1"] );
+
+    test.true(
+      ( call_11_1_block === call_11_2_block )
+      && ( call_11_1_block === call_21_1_block )
+      && ( call_11_1_block < call_12_1_block )
+    );
+    test.log(`call_11_1_block: ${call_11_1_block}`);
+    test.log(`call_11_2_block: ${call_11_2_block}`);
+    test.log(`call_12_1_block: ${call_12_1_block}`);
+    test.log(`call_21_1_block: ${call_21_1_block}`);
+  });
+  workspace.test('multicall by ft_transfer_call', async (test, {alice, bob, multicall, testHelper, testToken, root}) => {
+    let callError: any;
+    const multicallArgs: any = {
+      calls: [
+        [ 
+          {
+            address: testHelper.accountId,
+            actions: [
+              {
+                func: "log",
+                args: encodeBase64(JSON.stringify( { msg: "call_11_1" } )),
+                gas: "5000000000000",
+                depo: "0"
+              },
+              {
+                func: "log",
+                args: encodeBase64(JSON.stringify( { msg: "call_11_2" } )),
+                gas: "5000000000000",
+                depo: "0"
+              }
+            ]
+          },
+          {
+            address: testHelper.accountId,
+            actions: [
+              {
+                func: "log",
+                args: encodeBase64(JSON.stringify( { msg: "call_12_1" } )),
+                gas: "5000000000000",
+                depo: "0"
+              }
+            ]
+          }
+        ],
+        [
+          {
+            address: testHelper.accountId,
+            actions: [
+              {
+                func: "log",
+                args: encodeBase64(JSON.stringify( { msg: "call_21_1" } )),
+                gas: "5000000000000",
+                depo: "0"
+              }
+            ]
+          }
+        ]
+      ]
+    }
+    
+    /**
+     * test plan:
+     * 1- test token is not whitelisted:
+     *   a/ do multicall with bob (non-admin) => should fail because token not whitelisted
+     * 2- whitelist the test token
+     *   a/ do multicall with bob (non-admin) => should fail because bob is not an admin
+     *   b/ do multicall with alice (admin) => should succeed
+     */
+
+    // 1- a/
+    try{
+      await bob.createTransaction(testToken.accountId).functionCall(
+        "mint",
+        { account_id: bob.accountId, amount: "10" }
+      ).functionCall(
+        "ft_transfer_call",
+        { 
+          receiver_id: multicall.accountId,
+          amount: "1",
+          msg: `${{
+            function_id: "multicall",
+            args: encodeBase64(JSON.stringify( MulticallArgs ))
+          }}`
+        },
+        { gas: Gas.parse('150 Tgas'), attachedDeposit: NEAR.from('1') } // 1 yocto
+      ).signAndSend();
+  } catch(error) { console.log(error) }
+
   });
 
 }
